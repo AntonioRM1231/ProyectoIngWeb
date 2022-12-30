2577ca5ab96a1cb7775ac79bf30c9329f886bce9
AntonioRM1231 <rodriguezantony1231@gmail.com>
2022-12-30 03:04:34 -0600

    inicios php

--- ProyectoIngWeb/index.php
+++ ProyectoIngWeb/index.php
@@ -14,37 +14,9 @@
     <link rel="stylesheet" href="build/css/app.css">
 </head>
 <body>
-    <header class="header">
-        <div class="contenedor contenido-header">
-            <nav class="navegacion">
-                <!--CATEGORIAS-->
-                <a href="hombre.html">Hombre</a>
-                <a href="mujer.html">Mujer</a>
-                <a href="nino.html">Niño</a>
-                <a href="nina.html">Niña</a>
-                <a href="nosotros.html">Nosotros</a>
-                <!--LOGOTIPO-->
-                <a href="index.html">
-                    <img src="imagenes/ccm.png" alt="logotipo">
-                </a>
-                <!--INTERACTIVOS-->
-                <a href="#" data-bs-toggle="offcanvas" data-bs-target="#demo">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
-                        <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
-                    </svg>
-                    Iniciar sesión
-                </a>
-                <a href="#" data-bs-toggle="offcanvas" data-bs-target="#demo2">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-people-fill" viewBox="0 0 16 16">
-                        <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
-                        <path fill-rule="evenodd" d="M5.216 14A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216z"/>
-                        <path d="M4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z"/>
-                    </svg>
-                    Registrate
-                </a>
-            </nav>
-        </div>
-    </header>
+    <?php
+        include './includes/templates/header.php';
+    ?>
     <div class="offcanvas offcanvas-end" id="demo">
         <div class="offcanvas-header">
             <h1>
@@ -101,26 +73,50 @@
     </div>
     <hr>
     <main class="contenedor">
-        <h2>MUJER</h2>
+        <h2>NUESTROS PRODUCTOS</h2>
+        <section class="seccion contenedor">
+          <div class="contenedor-anuncio">
+              <a href="anuncio.html" class="anuncio">
+                  <picture>
+                      <source srcset="imagenes/forum1.png" type="image/jpg">
+                      <img loading="lazy" src="imagenes/forum1.png" alt="anuncio">
+                  </picture>
+                  <div class="contenido-anuncio">
+                      <h3>ADIDAS FORUM LOW EXHIBIT</h3>
+                      <p class="precio"><b> $1,200.00</b></p>
+                  </div>
+              </a>
+          </div>
+        </section>
     </main>
-    <section class="seccion contenedor">
-        <div class="contenedor-anuncio">
-            <a href="anuncio.html" class="anuncio">
-                <picture>
-                    <source srcset="imagenes/forum1.png" type="image/jpg">
-                    <img loading="lazy" src="imagenes/forum1.png" alt="anuncio">
-                </picture>
-                <div class="contenido-anuncio">
-                    <h3>ADIDAS FORUM LOW EXHIBIT</h3>
-                    <p class="precio"><b> $1,200.00</b></p>
-                </div>
-            </a>
+    <hr>
+    <section id="nosotros">
+      <div class="contenedor-nosotros">
+        <div class="img-nosotros">
+          <picture>
+            <source srcset="imagenes/catsnk.jpg" type="image/jpg">
+            <img loading="lazy" src="imagenes/catsnk.jpg" alt="anuncio">
+          </picture>
         </div>
+        <div class="desc-nosotros">
+          <h3>NOSOTROS</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus mauris nisl, 
+            cursus feugiat sapien ac, facilisis mollis enim. Aenean bibendum urna id ante varius 
+            interdum. In vulputate lacus in pulvinar suscipit. Pellentesque posuere varius 
+            vestibulum. Pellentesque sapien nisl, malesuada non sapien at, mattis elementum 
+            diam. Donec nisi quam, ultrices ut pharetra eget, pharetra in mauris. Nullam ullamcorper, 
+            nunc ac dignissim luctus, ante massa tempor nunc, non aliquet tellus quam sed eros. Quisque gravida, 
+            diam a molestie ultrices, ex neque pretium felis, a tempor diam leo sed orci. Pellentesque 
+            id metus quis neque laoreet placerat. Quisque commodo, diam ac accumsan sollicitudin, 
+            odio libero eleifend enim, in pretium odio felis eu elit.</p>
+        </div>
+      </div>
     </section>
     <hr>
     <footer  class="site-footer">
         <p><b>CUIDADO CON EL MICHI</b></p>
         <p>TODOS LOS DERECHOS RESERVADOS</p>
     </footer>
+    <script src="build/js/app.js"></script>
 </body>
 </html>
